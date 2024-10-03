455b296cd66c56e6283ae68d42e61d81cb25d7c5
Des1218 <eiliyahazzahrajasmin@yahoo.com>
2024-10-03 08:14:29 +0800

    Add files via upload

---
+++ Exercise-6-main/footer.php
@@ -0,0 +1,5 @@
+<?php
+?>
+<div class="copyright">
+    <p>&copy; BSIT-3D of PAMANTASAN NG LUNGSOD NG MUNTINLUPA. All Rights Reserved.</p>
+</div>
---
+++ Exercise-6-main/header.php
@@ -0,0 +1,9 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+?>
+
+<header>
+    <h1>Meet Our Team</h1>
+</header>
